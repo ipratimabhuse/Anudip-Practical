225e6847bc8bfa93e573cc32461e4756b374db55
ipratimabhuse <95672616+ipratimabhuse@users.noreply.github.com>
2022-01-11 12:20:34 +0530

    Add files via upload

---
+++ C# Nested Structure/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+struct student
+{
+    public String Firstname, Lastname, Address;
+}
+struct Marks
+{
+    public student s;
+    public int math, sci, eng, physics, hindi;
+}
+class nest
+{
+    public static void Main()
+    {
+        Marks m;
+        m.s.Firstname = "Pratima";
+        m.s.Lastname = "Bhuse";
+        m.s.Address = "Latur";
+        m.math = 85;
+        m.sci = 70;
+        m.eng = 80;
+        m.physics = 84;
+        m.hindi = 75;
+        Console.WriteLine("First Name = " + m.s.Firstname);
+        Console.WriteLine("Last Name = " + m.s.Lastname);
+        Console.WriteLine("Address=" + m.s.Address);
+        Console.WriteLine("Math Marks = " + m.math);
+        Console.WriteLine("Science = " + m.sci);
+        Console.WriteLine("Physics = " + m.physics);
+        Console.WriteLine("Hindi = " + m.hindi);
+    }
+}
+
---
+++ C# Structure/values.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Enumvariables
+{
+    public enum Values
+    {
+        a,b,c,d,e,f,g,h,i
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Value of D:");
+        int values = (int)Values.d;
+        Console.WriteLine(values);
+    }
+}
